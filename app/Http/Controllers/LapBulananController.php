42bf3e59a2a40cc07e8eb98fd2008c3ea52636e1
a.laode3302 <a.laode3302@gmail.com>
2024-08-09 00:48:32 +0000

    pesan commit

---
+++ app/Http/Controllers/LapBulananController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LapBulanan;
+use Illuminate\Http\Request;
+
+class LapBulananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LapBulanan $lapBulanan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LapBulanan $lapBulanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LapBulanan $lapBulanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LapBulanan $lapBulanan)
+    {
+        //
+    }
+}
---
+++ resources/views/kegiatan/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('template.main')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Halaman Edit Kegiatan</h1>
+        </div>
+    </div>
+</div>
+@endsection
